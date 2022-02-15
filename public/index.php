761598d6c13b3aedd96b7b64803a31b93056d35c
NektoUa <illyabarkov@gmail.com>
2022-02-15 11:40:20 +0200

    Group middleware

--- public/index.php
+++ public/index.php
@@ -4,22 +4,35 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$mw = function (Request $request, RequestHandler $handler) {
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello1 World');
+    return $response;
+});
+
+$app->group('/utils', function (RouteCollectorProxy $group) {
+    $group->get('/date', function (Request $request, Response $response) {
+        $response->getBody()->write(date('Y-m-d H:i:s'));
+        return $response;
+    });
+
+    $group->get('/time', function (Request $request, Response $response) {
+        $response->getBody()->write((string)time());
+        return $response;
+    });
+})->add(function (Request $request, RequestHandler $handler) use ($app) {
     $response = $handler->handle($request);
-    $response->getBody()->write('World');
+    $dateOrTime = (string) $response->getBody();
+
+    $response = $app->getResponseFactory()->createResponse();
+    $response->getBody()->write('It is now ' . $dateOrTime . '. Enjoy!');
 
     return $response;
-};
-
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write('Hello ');
-
-    return $response;
-})->add($mw);
+});
 
 $app->run();
