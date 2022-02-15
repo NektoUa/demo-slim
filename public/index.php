fc15a50f59948fbb0af83203e624bb6854e16dda
NektoUa <illyabarkov@gmail.com>
2022-02-15 11:37:12 +0200

    Route middleware

--- public/index.php
+++ public/index.php
@@ -1,33 +1,25 @@
 <?php
 
+use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Factory\AppFactory;
-use Slim\Psr7\Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->add(function (Request $request, RequestHandler $handler) {
+$mw = function (Request $request, RequestHandler $handler) {
     $response = $handler->handle($request);
-    $existingContent = (string) $response->getBody();
-
-    $response = new Response();
-    $response->getBody()->write('BEFORE ' . $existingContent);
+    $response->getBody()->write('World');
 
     return $response;
-});
-
-$app->add(function (Request $request, RequestHandler $handler) {
-    $response = $handler->handle($request);
-    $response->getBody()->write(' AFTER');
-    return $response;
-});
+};
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write('Hello World');
+    $response->getBody()->write('Hello ');
+
     return $response;
-});
+})->add($mw);
 
 $app->run();
